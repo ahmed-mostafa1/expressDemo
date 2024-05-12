9ab0f8170a9070e392620126906b21511bcc13d3
Ahmed Mostafa <sitesdesigner96@gmail.com>
2024-05-12 13:34:09 +0300

    using URL encoded body to send data

--- app.js
+++ app.js
@@ -15,14 +15,25 @@ app.listen(port, () => {
   console.log(`Hello from port ${port} ... `);
 });
 
+app.use(express.urlencoded({ extended: true }));
+
 // * HomePage
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/main.html"));
 });
 
-// * Get Welcome Page
+// * GET Welcome Page
 app.get("/welcome.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "/welcome.html"));
+  let fname = req.query.fnm;
+  let lname = req.query.lnm;
+
+  res.send(`Welcome ${fname} ${lname} From GET`);
+});
+// * POST Welcome Page
+app.post("/welcome.html", (req, res) => {
+  let fname = req.body.fnm;
+  let lname = req.body.lnm;
+  res.send(`Welcome ${fname} ${lname} From POST`);
 });
 // * Get specific Student
 app.get("/api/students/:id", (req, res) => {
