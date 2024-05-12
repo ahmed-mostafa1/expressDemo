3408d7e229bf7e8b6cd7a11ff20e2baf463937f4
Ahmed Mostafa <sitesdesigner96@gmail.com>
2024-05-12 16:17:07 +0300

    CRUD operations

--- app.js
+++ app.js
@@ -21,30 +21,72 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/main.html"));
 });
+app.get("/api/students/add", (req, res) => {
+  res.sendFile(path.join(__dirname, "/add.html"));
+});
 
+// ! SEND DATA via Query String
 // * GET Welcome Page
 app.get("/welcome.html", (req, res) => {
   let fname = req.query.fnm;
   let lname = req.query.lnm;
-
   res.send(`Welcome ${fname} ${lname} From GET`);
 });
-// * POST Welcome Page
-app.post("/welcome.html", (req, res) => {
-  let fname = req.body.fnm;
-  let lname = req.body.lnm;
-  res.send(`Welcome ${fname} ${lname} From POST`);
-});
-// * Get specific Student
+
+// * Getting DATA via HTTP {get}
 app.get("/api/students/:id", (req, res) => {
   let id = req.params.id;
   const student = Students.find((val, idx, arr) => {
     return val.id == id;
   });
-
   if (student) {
     res.json(student);
   } else {
     res.send("Student NOT Found!");
+  }
+});
+// * Getting DATA via HTTP {get}
+// Get All Students Data
+app.get("/api/students/", (req, res) => {
+  res.json(Students);
+});
+
+//* Create DATA via HTTP {post}
+// ! Not tested
+app.post("/api/students", (req, res) => {
+  req.body;
+  req.body.id = Students.length + 1;
+  Students.push(req.body);
+  res.send(Students);
+});
+
+// * Delete DATA via HTTP {delete}
+app.delete("/api/students/:id", (req, res) => {
+  let id = req.params.id;
+  let idx = Students.findIndex((val) => {
+    return val.id == id;
+  });
+  if (idx >= 0) {
+    Students.splice(idx, 1);
+    res.send("Student Deleted");
+  } else {
+    res.send("Student Not Found");
+  }
+});
+
+// * UPdate DATA via HTTP {put}
+// ! Not tested
+app.put("/api/students/:id", (req, res) => {
+  let id = req.params.id;
+  let idx = Students.findIndex((val) => {
+    return val.id == id;
+  });
+  if (idx >= 0) {
+    for (i in req.body) {
+      Students[idx][i] = req.body[i];
+    }
+    res.json(Students[idx]);
+  } else {
+    res.send("Student Not Found");
   }
 });
